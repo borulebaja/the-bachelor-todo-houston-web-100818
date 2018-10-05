dbcc8b9096b40d556d1ddcaa36a1bb2a5f656b54
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 05:30:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,16 @@ def get_first_name_of_season_winner(data, season)
     if season.to_s == season
       arr_contestants.each do |contestant_hash|
     
+    if contestant_hash[:status] == "Winner"
+          return contestant_hash[:name].split(' ').first
+        end
+      end
+      return "No Winner"
+    end
+  end
 end
+end
+
 
 def get_contestant_name(data, occupation)
   # code here
