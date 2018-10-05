d9b00f8f67086695b5a697c5c67273d4376c60be
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 05:27:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |data, arr_contestants|
     if season.to_s == season
-      
+      arr_contestants
 end
 
 def get_contestant_name(data, occupation)
