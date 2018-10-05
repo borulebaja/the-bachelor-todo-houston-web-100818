a2534101468ecfd28f0037680dc9afc1a7b8ee61
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 05:43:25 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data,season)
- data[season].each do |contestant|
-    if contestant["status"] == "Winner"
-      return contestant["name"].split(" ")[0]
-    end
-  end
-   
+ #data[season].each do |contestant|
+   # if contestant["status"] == "Winner"
+    #  return contestant["name"].split(" ")[0]
+  #  end
+  #end
+   data[season].find{|contestant| contestant["status"] == "Winner"}["name"].split.first
 end	 
 
 
