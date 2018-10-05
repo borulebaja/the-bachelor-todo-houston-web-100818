26567e895201bf0aedab04cc56af8cf3eb78cc3f
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 05:50:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,17 @@ end
 
 def get_contestant_name(data, occupation)
   data[occupation].each do |name|
-    
+    person = nil
+  data.each do |hash_season, contestants_array|
+    contestants_array.find do |contestant|
+      if contestant["occupation"] == occupation
+        person = contestant["name"]
+      end
+    end
+  end
+  person
+  
+end	end
 end
 
 def count_contestants_by_hometown(data, hometown)
