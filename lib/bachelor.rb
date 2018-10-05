9433ac0d5c3a40abe652d50d626fd6b567b580ac
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 06:12:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,8 @@ def count_contestants_by_hometown(data, hometown)
 end	
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, contestants_arr|
+
 end
 
 def get_average_age_for_season(data, season)
