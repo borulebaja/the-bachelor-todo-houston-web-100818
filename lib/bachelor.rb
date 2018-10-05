4caf2deb062c1289d42abaa204c9687cbd635b3d
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-05 05:35:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data,season)
- data[season.to_sym].each do |into_array|
+ data.each do |into_array|
    if into_array[:status] =="Winner"
      return into_array[:name].split(" ")[0]
    end
